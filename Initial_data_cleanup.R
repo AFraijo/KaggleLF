f80936884f3346610fea90fead17e6edf7cc11ec
Andrew Fraijo <afraijo@reefpointgroup.com>
2018-08-14 15:54:32 -0400

    Excellent progress with data tidying

--- Initial_data_cleanup.R
+++ Initial_data_cleanup.R
@@ -5,7 +5,10 @@ library(tidyverse)
 library(lubridate)
 library(feather)
 
-load("~/Projects/KaggleLF/Load_Forecasting.Rproj.RData")
+##load("Load_Forecasting.Rproj.RData")
+##bad habit, but let's save the data incase we mess up
+write_feather(Load_history, "Load_history.feather") 
+write_feather(temperature_history, "temperature_history.feather")
 
 ##this is not in a tidy format which makes some things difficult
 ##change date format
@@ -16,4 +19,28 @@ create_date <- function(YEAR, MONTH, DAY){
 }
 
 Load_history <- Load_history %>% 
-  mutate(Date = create_date(year, month, day))
+  mutate(Date = create_date(year, month, day)) %>%
+  mutate_at(vars(starts_with("h")), funs(as.character(.))) %>%
+  mutate_at(vars(starts_with("h")), funs(gsub(",", "", .))) %>%
+  mutate_at(vars(starts_with("h")), funs(as.numeric(.)))
+##funtions to create time stamps
+strip_time <- function(STRING){
+  hour1 <- substr(STRING,2,2)
+  hour2 <- as.character(as.numeric(hour1))
+  time <- paste(hour2,'00',sep=':')
+  return(time)
+}
+
+create_time_stamp <- function(DATE, HOUR){
+  time_stamp <- paste(DATE, HOUR, sep=" ") %>% ymd_hm()
+  return(time_stamp)
+}
+
+##Let's tidy this ****
+Load_history_Tidy <- Load_history %>%  
+  gather(key = "time", value = "kWh", starts_with("h")) %>% 
+  mutate(Hour = strip_time(time),
+         Time_stamp = create_time_stamp(Date, Hour)) %>% 
+  select(-year, -month, -day, -Date, - time, -Hour)
+
+
