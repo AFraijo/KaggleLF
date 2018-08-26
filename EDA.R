0d6ad670ccc51474c346deda10b7cbeff98101fd
AFraijo <andrew.fraijo@gmail.com>
2018-08-26 00:39:14 -0400

    Added graph for subset to show small variations

--- EDA.R
+++ EDA.R
@@ -29,25 +29,62 @@ All_data_tidy <- All_data_tidy %>%
 ##         Day = wday(Time_stamp, label = TRUE, abbr = FALSE),
 ##         Hour = hour(Time_stamp))
 
-
+cols <- c("10" = "#E69F00", "25" = "#56B4E9", "50" = "#F0E442", "75" = "#009E73", "90" = "#CC79A7")
 
 
 All_data_tsbl <- as_tsibble(All_data_tidy, key = id(zone_id), index = Time_stamp)
 
-All_data_tsbl %>% group_by(zone_id) %>% 
+YM_quartile <- All_data_tsbl %>% group_by(zone_id) %>% filter(!is.na(kWh)) %>% 
   index_by(Month = yearmonth(Time_stamp)) %>% 
-  summarize(Ave_kWh = mean(kWh, na.rm = TRUE)) %>% 
-  ggplot(aes(x = Month, y = Ave_kWh)) + geom_line() +
-  facet_wrap(~zone_id, ncol = 5) + 
+  summarize(`10` = quantile(kWh, probs = 0.1),
+            `25` = quantile(kWh, probs = 0.25),
+            `50` = quantile(kWh, probs = 0.5),
+            `75` = quantile(kWh, probs = 0.75),
+            `90` = quantile(kWh, probs = 0.9))
+
+YM_Scatter <- All_data_tsbl %>% index_by(Month = yearmonth(Time_stamp)) %>% 
+  filter(!is.na(kWh)) %>% ggplot(aes(x = Month, y = kWh)) + 
+  geom_point(aes(group = zone_id)) + geom_jitter(size = .1, alpha = 0.1) +
+  geom_line(data = YM_quartile, aes(x = Month, y = `10`, group = zone_id, colour = "10")) +
+  geom_line(data = YM_quartile, aes(x = Month, y = `25`, group = zone_id, colour = "25")) +
+  geom_line(data = YM_quartile, aes(x = Month, y = `50`, group = zone_id, colour = "50")) +
+  geom_line(data = YM_quartile, aes(x = Month, y = `75`, group = zone_id, colour = "75")) +
+  geom_line(data = YM_quartile, aes(x = Month, y = `90`, group = zone_id, colour = "90")) +
+  facet_wrap( ~ zone_id, ncol = 4) + 
   theme_bw() + 
-  theme(axis.text.x = element_text(angle = 90))
-  facet_wrap(~zone_id, ncol = 5)
+  labs(title = "Year - Month Energy Use By Zone",
+       subtitle = "With Quantile Curves") + 
+  scale_colour_manual(name = "Quantile", values = cols)+ 
+  theme(axis.text.x = element_text(angle = 90), legend.position = "bottom")
+
+YM_Scatter
+
+##Try pulling 1,4,5,8 into one group? Then 9,10,13,14?
+YM_Q_limited <- YM_quartile %>% filter(zone_id %in% c(1,4,5,8))
+
+YM_Limited <- All_data_tsbl%>% filter(zone_id %in% c(1,4,5,8), !is.na(kWh)) %>% 
+  index_by(Month = yearmonth(Time_stamp)) %>% 
+  ggplot(aes(x = Month, y = kWh)) + geom_point(aes(group = zone_id)) + 
+  geom_jitter(size = .1, alpha = 0.1) +
+  geom_line(data = YM_Q_limited, aes(x = Month, y = `10`, group = zone_id, colour = "10")) +
+  geom_line(data = YM_Q_limited, aes(x = Month, y = `25`, group = zone_id, colour = "25")) +
+  geom_line(data = YM_Q_limited, aes(x = Month, y = `50`, group = zone_id, colour = "50")) +
+  geom_line(data = YM_Q_limited, aes(x = Month, y = `75`, group = zone_id, colour = "75")) +
+  geom_line(data = YM_Q_limited, aes(x = Month, y = `90`, group = zone_id, colour = "90")) +
+  facet_wrap(~ zone_id, ncol = 1) + 
+  theme_bw() + 
+  labs(title = "Year - Month Energy Use By Zone",
+       subtitle = "With Quantile Curves") + 
+  scale_colour_manual(name = "Quantile", values = cols)+ 
+  theme(axis.text.x = element_text(angle = 90), legend.position = "bottom")
+
+YM_Limited
 
 ##This is ok, but wonky. Let's try a regular dplyr approach
 All_data_tidy %>% group_by(zone_id, Month) %>% 
   summarize(Ave_kWh = mean(kWh, na.rm = TRUE)) %>% 
   ggplot(aes(x = Month, y = Ave_kWh)) + geom_line(aes(group = zone_id)) +
-  facet_wrap(~zone_id, ncol = 5) + 
+  facet_wrap(~ zone_id, ncol = 5) + 
   theme_bw() + 
   theme(axis.text.x = element_text(angle = 90))
 
@@ -62,8 +99,6 @@ Monthly_quantiles <- All_data_tidy %>% filter(!is.na(kWh)) %>%
             `75` = quantile(kWh, probs = 0.75),
             `90` = quantile(kWh, probs = 0.9)) 
 
-##add jitter and labels
-cols <- c("10" = "#E69F00", "25" = "#56B4E9", "50" = "#F0E442", "75" = "#009E73", "90" = "#CC79A7")
 
 Monthly_Scatter <- All_data_tidy %>% filter(!is.na(kWh)) %>% 
   select(zone_id, Month, kWh) %>% 
@@ -73,7 +108,7 @@ Monthly_Scatter <- All_data_tidy %>% filter(!is.na(kWh)) %>%
     geom_line(data = Monthly_quantiles, aes(x = Month, y = `50`, group = zone_id, colour = "50")) +
     geom_line(data = Monthly_quantiles, aes(x = Month, y = `75`, group = zone_id, colour = "75")) +
     geom_line(data = Monthly_quantiles, aes(x = Month, y = `90`, group = zone_id, colour = "90")) +
-  facet_wrap(~zone_id, ncol = 5) + 
+  facet_wrap(~ zone_id, ncol = 5) + 
   theme_bw() + 
   labs(title = "Monthly Energy Use By Zone",
        subtitle = "With Quantile Curves") + 
@@ -112,7 +147,7 @@ Daily_Scatter <- All_data_tidy %>% filter(!is.na(kWh)) %>% select(zone_id, Day, 
   geom_line(data = Daily_quantiles, aes(x = Day, y = `50`, group = zone_id, colour = "50")) +
   geom_line(data = Daily_quantiles, aes(x = Day, y = `75`, group = zone_id, colour = "75")) +
   geom_line(data = Daily_quantiles, aes(x = Day, y = `90`, group = zone_id, colour = "90")) +
-  facet_wrap(~zone_id, ncol = 5) + 
+  facet_wrap(~ zone_id, ncol = 5) + 
   theme_bw() + 
   labs(title = "Zone Energy Use By Day of Week",
        subtitle = "With Quantile Curves") + 
@@ -140,14 +175,14 @@ Hourly_Scatter <- All_data_tidy %>% filter(!is.na(kWh)) %>% select(zone_id, Hour
   geom_line(data = Hourly_quantiles, aes(x = Hour, y = `50`, group = zone_id, colour = "50")) +
   geom_line(data = Hourly_quantiles, aes(x = Hour, y = `75`, group = zone_id, colour = "75")) +
   geom_line(data = Hourly_quantiles, aes(x = Hour, y = `90`, group = zone_id, colour = "90")) +
-  facet_wrap(~zone_id, ncol = 5) + 
+  facet_wrap(~ zone_id, ncol = 5) + 
   theme_bw() + 
   labs(title = "Zone Energy Use By Hour",
        subtitle = "With Quantile Curves") + 
   scale_colour_manual(name = "Quantile",  values = cols)+ 
   theme(axis.text.x = element_text(angle = 90), legend.position = "bottom") 
 
-##Hourly_Scatter
+Hourly_Scatter
 ##Also interesting
 
 ##attempt to decompose
