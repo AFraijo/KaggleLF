3c20d1bf925a1f55513daae2511c9269405e0e9b
Andrew Fraijo <afraijo@reefpointgroup.com>
2018-09-10 18:44:04 -0400

    Additional work.

--- Basic_forecast.R
+++ Basic_forecast.R
@@ -11,8 +11,11 @@ options(scipen = 100000000)
 
 Backcast_data <- All_data_tsbl %>% filter(is.na(kWh))
 
+Zone_1_test <-  All_data_tidy %>% filter(zone_id == 1, Time_stamp >= as_datetime("2008-1-1 00:00:00"))
 
-Zone_1_msts <- All_data_tidy %>% filter(zone_id == 1, !is.na(kWh)) %>% select(kWh) %>% 
+Zone_1_train <- All_data_tidy %>% filter(zone_id == 1, Time_stamp < as_datetime("2008-1-1 00:00:00"))
+
+Zone_1_msts <- All_data_tidy %>% filter(zone_id == 1, !is.na(kWh), Time_stamp < as_datetime("2008-1-1 00:00:00")) %>% select(kWh) %>% 
   msts(seasonal.periods = c(24,168,8766), start = decimal_date(as_datetime("2004-01-01 01:00:00"
   )))
 ##Zone_1_mstl <- Zone_1_msts %>% mstl(s.window = "periodic", robust = TRUE)
@@ -30,3 +33,16 @@ Zone_1_ARIMA %>% autoplot(include = 12419, xlab = "Year", ylab = "Energy Demand 
 
 Zone_1_ETS <- Zone_1_msts %>% stlf(s.window = "periodic", robust = TRUE,method="ets", lambda = "auto")
 Zone_1_ETS %>% autoplot(include = 12419, xlab = "Year", ylab = "Energy Demand (kWh)") + theme_bw()
+
+
+##xreg assumes the errors of a regression are ARIMA
+Zone_1_xreg_check <- auto.arima(Zone_1_train[,"kWh"], xreg = Zone_1_train[,"Average"])
+Zone_1_xreg_check %>% checkresiduals()
+##It's not bad! There are some lags that are high on the ACF plot, but the residuals look Gaussian
+##unfortunately to actually
+Zone_1_xreg <- forecast(Zone_1_train[,"kWh"], xreg = Zone_1_test[,"Average"])
+
+
+
+
+
